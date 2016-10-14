5dcfb9c8144168da700d709a49c3ef337fe4d4dc
sengmitnick <sengmitnick@163.com>
2016-10-15 02:15:18 +0800

    修改
    在我系统环境下多处出错，修改成可以使用的

--- Typecho2Hexo.py
+++ Typecho2Hexo.py
@@ -9,60 +9,55 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+# 把数据库相关信息改成自己的，默认localhost（和数据库同机的话不需要修改）
+db = torndb.Connection(host="localhost", database="数据库", user="root", password="密码")
+# 创建分类和标签
+categories = db.query("select type, slug, name from typecho_metas")
+for cate in categories:
+    path = 'data/%s' % cate.slug
+    if not os.path.exists(path):
+        os.makedirs(path)
+    f = codecs.open('%s/index.md' % path, 'w', "utf-8")
+    f.write("title: %s\n" % cate.slug)
+    f.write("date: %s\n" % arrow.now().format('YYYY-MM-DD HH:mm:ss'))
+    # 区分分类和标签
+    if cate.type == 'category':
+        f.write('type: "categories"\n')
+    elif cate.type == 'tags':
+        f.write('type: "tags"\n')
+    # 禁止评论
+    f.write("comments: false\n")
+    f.write("---\n")
+    f.close()
 
-def create_data(db):
-    # 创建分类和标签
-    categories = db.query("select type, slug, name from typecho_metas")
-    for cate in categories:
-        path = 'data/%s' % cate.slug
-        if not os.path.exists(path):
-            os.makedirs(path)
-        f = codecs.open('%s/index.md' % path, 'w', "utf-8")
-        f.write("title: %s\n" % cate.slug)
-        f.write("date: %s\n" % arrow.now().format('YYYY-MM-DD HH:mm:ss'))
-        # 区分分类和标签
-        if cate.type == 'category':
-            f.write('type: "categories"\n')
-        elif cate.type == 'tags':
-            f.write('type: "tags"\n')
-        # 禁止评论
-        f.write("comments: false\n")
-        f.write("---\n")
-        f.close()
-
-    # 创建文章
-    entries = db.query("select cid, title, slug, text, created from typecho_contents where type='post'")
-    for e in entries:
-        title = e.title.encode("utf-8")
-        content = str(e.text.encode("utf-8")).replace('<!--markdown-->', '')
-        tags = []
-        category = ""
-        # 找出文章的tag及category
-        metas = db.query(
-            "select type, name, slug from `typecho_relationships` ts, typecho_metas tm where tm.mid = ts.mid and ts.cid = %s",
-            e.cid)
-        for m in metas:
-            if m.type == 'tag':
-                tags.append(m.name)
-            if m.type == 'category':
-                category = m.slug
-        path = 'data/_posts/'
-        if not os.path.exists(path):
-            os.makedirs(path)
-        f = codecs.open('%s%s.md' % (path, cid), 'w', "utf-8")
-        f.write("title: %s\n" % title)
-        f.write("date: %s\n" % arrow.get(e.created).format('YYYY-MM-DD HH:mm:ss'))
-        f.write("categories: %s\n" % category)
-        f.write("tags: [%s]\n" % ','.join(tags))
-        f.write("---\n")
-        f.write(content)
-        f.close()
-
-
-def main():
-    # 把数据库相关信息改成自己的，默认localhost（和数据库同机的话不需要修改）
-    db = torndb.Connection(host="localhost", database="数据库名称", user="数据库用户", password="数据库密码")
-    create_data(db)
-
-if __name__ == "__main__":
-    main()
+# 创建文章
+entries = db.query("select cid, title, slug, text, created from typecho_contents where type='post'")
+for e in entries:
+    title = e.title.encode("utf-8")
+    cid = e.cid
+    content = e.text
+    if content is None:
+        content = ""
+    content = str(content.encode("utf-8")).replace('<!--markdown-->', '')
+    tags = []
+    category = ""
+    # 找出文章的tag及category
+    metas = db.query(
+        "select type, name, slug from `typecho_relationships` ts, typecho_metas tm where tm.mid = ts.mid and ts.cid = %s",
+        e.cid)
+    for m in metas:
+        if m.type == 'tag':
+            tags.append(m.name)
+        if m.type == 'category':
+            category = m.slug
+    path = 'data/_posts/'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    f = codecs.open('%s%s.md' % (path, cid), 'w', "utf-8")
+    f.write("title: %s\n" % title)
+    f.write("date: %s\n" % arrow.get(e.created).format('YYYY-MM-DD HH:mm:ss'))
+    f.write("categories: %s\n" % category)
+    f.write("tags: [%s]\n" % ','.join(tags))
+    f.write("---\n")
+    f.write(content)
+    f.close()
